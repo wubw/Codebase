34830b6ae45bcf1ff6068f29b4d49e16cd7fee97
Binwei <summersnowe@gmail.com>
2015-12-10 18:29:01 +0800

    First check in

---
+++ Codebase/Analyzer.fs
@@ -0,0 +1,34 @@
+﻿module Analyzer
+open System
+open System.IO
+
+let filter file =
+    let allowedExt = [|".fs"; ".cs"|]
+    let ext = Path.GetExtension file
+    Seq.contains ext allowedExt
+
+let rec search path = 
+    let ret = path |> Directory.EnumerateFiles
+    let ret2 = path |> Directory.EnumerateDirectories |> Seq.collect (fun x -> search(x)) 
+    Seq.append ret ret2 |> Seq.filter (fun x -> x |> filter)
+
+let processFile file = 
+    try
+        let allLines = file |> File.ReadAllLines
+        let notEmptyLines = Seq.filter (fun x -> x |> String.IsNullOrWhiteSpace |> not) allLines
+        let ret = (file, allLines.Length + 1, Seq.length notEmptyLines)
+        ret
+    with 
+        |_ -> (String.Empty, 0, 0)
+
+let start path = 
+    if Directory.Exists(path) then
+        let results = path |> search |> Seq.map (fun x -> x |> processFile)
+        printfn "Codebase: %A, %A" path results
+        let aggregateResult = results |> Seq.reduce (fun x y -> 
+            let (_, x2, x3) = x
+            let (_, y2, y3) = y
+            (String.Empty, x2+y2, x3+y3))
+        printfn "All lines and not empty lines: %A" aggregateResult 
+    else
+        printfn "Directory %A does not exist." path
---
+++ Codebase/Program.fs
@@ -0,0 +1,8 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv = 
+    match argv with
+    | [|path|] -> Analyzer.start path
+    | _ -> printfn "Please specify valid parameters."
+    0
